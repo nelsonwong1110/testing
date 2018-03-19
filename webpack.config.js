0f2770479e4b45e61e6dcc1ccce93a6222fd0908
Nelson Wong <nelsonwong@Nelsons-MacBook-Pro.local>
2018-03-20 00:06:43 +0800

    vjkv

--- webpack.config.js
+++ webpack.config.js
@@ -30,7 +30,7 @@ module.exports = {
         loader: 'babel-loader',
         exclude: /node_modules/,
         query: {
-            presets: ['react','es2015']
+            presets: ['es2015', 'react']
         }
       }
     ]
